1be318a8412d22c5e8985867bfee26bb25cea9f0
Josh Wilkerson <jgwilkerson@gmail.com>
2019-06-27 13:35:36 -0500

    conditional rendering in sample component

--- src/sample-components/clock.js
+++ src/sample-components/clock.js
@@ -19,10 +19,20 @@ class Clock extends React.Component {
 
 	render() {
 		const { date } = this.state
+		const hours = date.getHours()
+		let timeOfDay
+
+		if (hours < 12) {
+			timeOfDay = 'morning'
+		} else if (hours >= 12 && hours < 17) {
+			timeOfDay = 'afternoon'
+		} else {
+			timeOfDay = 'night'
+		}
 
 		return (
 			<div>
-				<h1>Hello, world!</h1>
+				<h1>Good {timeOfDay}!</h1>
 				<h2>It is {date.toLocaleTimeString()}.</h2>
 			</div>
 		)
